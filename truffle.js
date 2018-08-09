15f2e58e9ecbfeef5a1b3968252acaef2a1aa8b8
nervehammer <nervehammer.suraj@gmail.com>
2018-08-09 19:01:01 +0530

    added basic scripts

--- truffle.js
+++ truffle.js
@@ -1,40 +1,21 @@
-//var HDWalletProvider = require("truffle-hdwallet-provider");
-
-//var mnemonic = "word vocal hazard glory home property canvas duty fetch private wasp ozone";
-
 module.exports = {
   networks: {
     development: {
-      host: "176.9.155.139",
-      port: 8069,
-      network_id: "5777"
-    }/*,
-    ropsten: {
-      gasPrice : 1,
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/");
-      },      
-      network_id: 3
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
     },
-    rinkeby: {
-      gasPrice : 1,
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/");
-      },      
-      network_id: 4
+    coverage: {
+      host: 'localhost',
+      network_id: '*', 
+      port: 8555,
+      gas: 0x67C280,
+      gasPrice: 0x01,
     },
-    kovan: {
-      gasPrice : 1,
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/");
-      },      
-      network_id: 42
-    } */
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
+    ganache: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+    },
   },
 };
